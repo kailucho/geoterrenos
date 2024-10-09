c71572361054b21a8ed8d26935f145d3f96772aa
Luijhy Guerra <qdsjguerra@utp.edu.pe>
2024-10-08 20:00:02 -0500

    cambio a variables de entorno[

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,14 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+// Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-  apiKey: "AIzaSyAU79hhkQyXR1yoxV0CHAZ-iP_Rf_BpPrY",
-  authDomain: "landing-map-70309.firebaseapp.com",
-  projectId: "landing-map-70309",
-  storageBucket: "landing-map-70309.appspot.com",
-  messagingSenderId: "608132779744",
-  appId: "1:608132779744:web:eae50f8d4caa920f204a85",
-  measurementId: "G-0K35MWVZWH",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inicializa Firebase
